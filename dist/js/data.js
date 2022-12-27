471b0ada5c8fd80ec8dbbbd0b4d67939bad0443a
hassanShakur <hass.shakurhassan@gmail.com>
2022-12-27 14:07:07 +0300

    Js data

---
+++ dist/js/data.js
@@ -0,0 +1,18 @@
+const acc1 = {
+  owner: 'Abba Beth',
+  transactions: [200, 450, -400, 3000, -650, -100, 70, 1420],
+  interestRate: 1.3,
+  pass: 111,
+};
+const acc2 = {
+  owner: 'Chad Davinci',
+  transactions: [200, -200, 440, 3000, 650, -500, -70, 1420, -800],
+  interestRate: 1.4,
+  pass: 222,
+};
+const acc3 = {
+  owner: 'Eunice Foster',
+  transactions: [5500, -210, 350, -300, 30, -15, 70, 8480, -60],
+  interestRate: 0.8,
+  pass: 333,
+};

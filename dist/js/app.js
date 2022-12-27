f5e472c899c61de668ad9f6333783290f39089f7
hassanShakur <hass.shakurhassan@gmail.com>
2022-12-27 14:32:02 +0300

    Get usernames

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,2 +1,18 @@
 import { acc1, acc2, acc3 } from './data.js';
-console.log(acc3);
+
+const accounts = [acc1, acc2, acc3];
+
+// * Get name initials from accounts
+
+const getUserName = function (accs) {
+  accs.forEach(function (acc) {
+    acc.userName = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map((name) => name[0])
+      .join('');
+  });
+};
+
+getUserName(accounts);
+// console.log(acc1.userName);

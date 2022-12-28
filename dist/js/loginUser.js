973fb10baf08559d0f7dde3d35d78262311f13e4
hassanShakur <hass.shakurhassan@gmail.com>
2022-12-28 08:46:54 +0300

    Create show notification

--- dist/js/loginUser.js
+++ dist/js/loginUser.js
@@ -1,7 +1,6 @@
 import get from './getElement.js';
 import updateUI from './updateUI.js';
 
-
 let activeUser;
 
 const inputUserName = get('.user-name');
@@ -25,13 +24,25 @@ const loginUser = (accounts) => {
     // ? Greet user
     greetings.innerHTML = `good <span class="greet-time">afternoon, </span
           ><span class="name">${activeUser.owner.split(' ')[0]}</span>`;
-  }
 
-  // ! Reset login inputs
-  inputPassword.value = inputUserName.value = '';
-  inputPassword.blur();
+    // ! Reset login inputs
+    inputPassword.value = inputUserName.value = '';
+    inputPassword.blur();
+  } else {
+    showNotification('error', 'wrong username or password');
+  }
 
   return activeUser;
 };
+
+function showNotification(type, message) {
+  const notifier = document.querySelector('.notification');
+  notifier.textContent = message;
+  notifier.style.backgroundColor = type === 'error' ? 'red' : 'green';
+  notifier.classList.add('show-notification');
+  setTimeout(() => {
+    notifier.classList.remove('show-notification');
+  }, 1400);
+}
 
 export default loginUser;
